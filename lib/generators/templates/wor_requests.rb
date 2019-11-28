8adedf5e1545157abb56f68497ba984b10d5a07e
Tomas Abel Gonzalez <tomasabelgonzalez@protonmail.com>
2019-11-28 10:45:30 -0300

    Added options for avoiding logs

--- lib/generators/templates/wor_requests.rb
+++ lib/generators/templates/wor_requests.rb
@@ -1,3 +1,12 @@
 Wor::Requests.configure do |config|
   config.logger = Rails.logger
+
+  if Rails.env.test?
+    LoggerMock = Struct.new(nil) do
+      def info(str); end
+
+      def error(str); end
+    end
+    config.logger = LoggerMock.new
+  end
 end
